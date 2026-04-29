d3912d1d211d85975cd48278becfed70fc644928
Vansh Rana <vanshrana98050@gmail.com>
2026-04-29 22:53:10 +0530

    Added java codes
    hello world

---
+++ sumarray.java
@@ -0,0 +1,11 @@
+public class sumarray{
+    public static void main (String [] args){
+        int arr[] = new int[5];
+
+        arr[1]=4;
+        arr[0]=3;
+        arr[4]=9;
+        System.out.println(arr[1]);
+     System.out.println(arr[1]+arr[4]);
+        }
+    }

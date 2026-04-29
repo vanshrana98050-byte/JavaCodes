81f345589bd9c6a72f6aeb9827c34b22b671a7f1
Vansh Rana <vanshrana98050@gmail.com>
2026-04-30 00:11:32 +0530

    Add files via upload

--- sumarray.java
+++ sumarray.java
@@ -1,11 +1,12 @@
 public class sumarray{
-    public static void main (String [] args){
-        int arr[] = new int[5];
-
-        arr[1]=4;
-        arr[0]=3;
-        arr[4]=9;
-        System.out.println(arr[1]);
-     System.out.println(arr[1]+arr[4]);
-        }
+    public static void main(String[]args){
+        int arr[]=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        System.out.println(arr[1]+arr[4]
+        );
     }
+}

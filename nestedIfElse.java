cbb135be6db3b6d4fe906f9fd54da331d6cd7fbb
Vansh Rana <vanshrana98050@gmail.com>
2026-04-30 23:28:12 +0530

    Add files via upload

---
+++ nestedIfElse.java
@@ -0,0 +1,13 @@
+public class nestedIfElse{
+    public static void main (String[]args){
+        int num= 8;
+        if (num>=5){
+            if(num>6){
+                System.out.println("it is a big number");
+            }
+            else{
+                System.out.println("it is not 6 ");
+            }
+        }
+    }
+}

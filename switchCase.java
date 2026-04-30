74ea9920ce645a3b8d2813014552cff834f61eda
Vansh Rana <vanshrana98050@gmail.com>
2026-04-30 23:48:03 +0530

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,14 @@
+public class switchCase{
+    public static void main(String[]args){
+        int num = 1;
+        switch (num) {
+            case 1: System.out.println("ONE");
+            break;
+            case 2: System.out.println("TWO");
+            break;
+            case 3: System.out.println("THREE");
+            break;
+            default: System.out.println("unknown");
+        }
+    }
+}
